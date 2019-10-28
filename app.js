78684356f7c6f53d7731d7201a053fcb78a2695f
Maximiliano Vergara <maximilianoezequielvergara@gmail.com>
2019-10-28 02:50:59 -0300

    fix CORS

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const api = require('./routes/api.routes');
+const cors = require('cors');
+const corsOptions = {
+    origin: 'http://localhost:4200',
+  }
 
 //settings
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 app.use('/api', api);
 
 module.exports = app;
